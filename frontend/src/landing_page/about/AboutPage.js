d00509f7bae927bc9f9af97ab54f6c1c67791ddb
praveen-gouraj <praveengouraj885@gmail.com>
2026-07-20 12:30:32 +0530

    completed the frontend part

--- frontend/src/landing_page/about/AboutPage.js
+++ frontend/src/landing_page/about/AboutPage.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Hero from './Hero';
-import Team from './Team';
+import React from "react";
 
-function AboutPage() {
-    return ( 
-        <>   
-            <Hero />
-            <Team />
-        </>
-     );
+import Hero from "./Hero";
+import Team from "./Team";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
 }
 
-export default AboutPage;
+export default PricingPage;
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Hero from './Hero';
-import CreateTicket from './CreateTicket';
+import React from "react";
 
-function SupportPage() {
-    return ( 
-        <>
-        <Hero/>
-        <CreateTicket/>
-        </>
-     );
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
-export default SupportPage;
+export default PricingPage;
